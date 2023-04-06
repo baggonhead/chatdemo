32558afe634fafff114e183915917ae4d59c5e80
baggonhead <lolcakezz15@gmail.com>
2023-04-05 21:22:33 -0400

    Add project files.

---
+++ chatdemo/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chatdemo.Hubs
+{
+    public class ChatHub :Hub
+    {
+        public async Task SendMessage(string username, string message)
+        {
+            await Clients.All.SendAsync("RecieveMessage", username, message);
+        }
+    }
+}
